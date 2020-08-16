30a4532140c6f2cb50d479fc596cf0d03134c545
Amir Dora <ad@netzlab.de>
2020-08-16 11:40:43 +0200

    first commit

---
+++ Blog/Data/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Repository
+{
+    public class Repository : IRepository
+    {
+        private AppDbContext _ctx;
+
+        public Repository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+
+        }
+
+        public void AddPost(Post post)
+        {
+            _ctx.Posts.Add(post);
+        }
+
+        public List<Post> GetAllPost()
+        {
+            return _ctx.Posts.ToList();
+        }
+
+        public Post GetPost(int id)
+        {
+            return _ctx.Posts.FirstOrDefault(m => m.Id == id);
+        }
+
+        public void RemovePost(int id)
+        {
+            _ctx.Posts.Remove(GetPost(id));
+        }
+
+        public void UpdatePost(Post post)
+        {
+            _ctx.Posts.Update(post);
+        }
+        public async Task<bool> SaveChangesAsync()
+        {
+            if (await _ctx.SaveChangesAsync() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
